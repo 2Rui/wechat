bb076cf91719a42b7aecf1f00f03490eeedf243b
2Rui <2698632709@qq.com>
2019-01-05 16:54:40 +0800

    index

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 App({
-  
+  onLaunch:function(){
+    console.log('onlaunch')
+    //先判断用户已经授权了那些信息
+    wx.getSetting({
+       success:res=>{
+         if (res.authSetting['scope.userInfo']){
+                //说明已经授权，可直接调用wx.getUserInfo获取用户信息
+                wx.getUserInfo({
+                  success:res=>{
+                    console.log(res)
+                    this.globalData.userInfo=res.userInfo;
+                    // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                    // 所以此处加入 callback 以防止这种情况
+                    if (this.userInfoReadyCallback) {
+                      this.userInfoReadyCallback(res)
+                    }
+                  }
+                })
+         }
+       }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,117 @@
+// pages/index/index.js
+//获取应用实例
+const app=getApp();
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
+    avatarUrl:'./user-unlogin.png',
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  onLoad() {
-    // 查看是否授权
-    wx.getSetting({
-      success(res) {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success(res) {
-              console.log(res.userInfo)
-            }
-          })
-        }
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //公共变量中用户信息是否存在
+  if(app.globalData.userInfo){
+    this.setData({
+      avatarUrl: app.globalData.userInfo.avatarUrl
+    })
+  }else if(this.canIUse){
+    app.userInfoReadyCallback = res => {
+      this.setData({
+        userInfo: res.userInfo,
+        hasUserInfo: true
+      })
+    }
+  }else{
+    // 在没有 open-type=getUserInfo 版本的兼容处理
+    wx.getUserInfo({
+      success:res=>{
+        app.globalData.userinfo=res.userInfo;
+        this.setData({
+          avatarUrl:res.userInfo.avatarUrl
+        })
+      },
+      fail:res=>{
+        console.log('授权失败')
       }
     })
+  }
   },
-  bindGetUserInfo(e) {
-    console.log(e.detail.userInfo)
+  selfGetInfo :function(e){
+    //用户点击了允许
+     if(e.detail.userInfo){
+       app.globalData.userinfo = e.detail.userInfo;
+       this.setData({
+         avatarUrl: e.detail.userInfo.avatarUrl
+       })
+     }else{
+       //用户点击了失败
+       wx.getUserInfo({
+         success: res => {
+           app.globalData.userinfo = res.userInfo;
+           this.setData({
+             avatarUrl: res.userInfo.avatarUrl
+           })
+         },
+         fail: res => {
+           console.log('授权失败')
+         }
+       })
+     }
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
